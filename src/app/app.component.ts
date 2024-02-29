b769811e825d0510d96961aafb4c2a5875b32092
Jorge1 <agullojorge@gmail.com>
2024-02-29 07:33:50 +0100

    [WIP]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'electric-cost-web-app';
+  title = 'Electric Cost. Hello';
+  enabled: boolean = false;
+
+  courses: string[] = ['Angular', 'React', 'Spring Boot'];
+
+  setEnabled(): void { this.enabled = !this.enabled; }
 }
