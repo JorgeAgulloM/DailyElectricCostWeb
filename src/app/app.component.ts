65ed1f5249d5af2047a670ff7d5dfac6fcd7392a
Jorge1 <agullojorge@gmail.com>
2024-03-03 09:52:47 +0100

    Implemented components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MediaComponent } from './components/media/media.component';
+import { AppFeaturesComponent } from './components/app-features/app-features.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, HeaderComponent, MediaComponent, AppFeaturesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
