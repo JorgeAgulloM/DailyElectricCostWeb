eb47b0bd71f2f41fec6d5c818bd993f08770ee42
Jorge1 <agullojorge@gmail.com>
2024-03-04 21:43:05 +0100

    WIP

--- src/app/components/media/media.component.ts
+++ src/app/components/media/media.component.ts
@@ -9,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class MediaComponent {
 
+  cardData = [
+    {
+      image: './assets/img/captures/image_01.jpg',
+      title: "Precios de un solo vistazo"
+    },
+    {
+      image: './assets/img/captures/image_02.jpg',
+      title: "Establece alertas"
+    },
+    {
+      image: './assets/img/captures/image_07.jpg',
+      title: "Moodo oscuro"
+    },
+    {
+      image: './assets/img/captures/image_04.jpg',
+      title: "Como se crea tu energía"
+    },
+    {
+      image: './assets/img/captures/image_05.jpg',
+      title: "Históarico diario"
+    },
+  ]
 }
