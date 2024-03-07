672073723147458bdf8463a5b7372556dcd6ffe3
Jorge1 <agullojorge@gmail.com>
2024-03-07 17:36:31 +0100

    Added urls Social-media

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -4,6 +4,6 @@ export const INSTAGRAM = 'instagram';
 
 /** URLs */
 export const GOOGLE_PLAY_APP_LINK = 'https://play.google.com/store/apps/details?id=com.softyorch.dailyelectriccost';
-export const TWITTER_ACCOUNT_LINK = ''
-export const YOUTUBE_ACCOUNT_LINK = ''
-export const INSTAGRAM_ACCOUNT_LINK = ''
+export const TWITTER_ACCOUNT_LINK = 'https://twitter.com/JorgeAgulloM'
+export const YOUTUBE_ACCOUNT_LINK = 'https://www.youtube.com/watch?v=zAzbj_OGmZU&ab_channel=SoftYorch'
+export const INSTAGRAM_ACCOUNT_LINK = 'https://www.instagram.com/yorchdevelop'
