f5667e25c5cce224b80ea74165cf79df1203afd8
Jorge1 <agullojorge@gmail.com>
2024-03-07 19:56:30 +0100

    Implemented zoom image

--- src/app/components/media/media.component.ts
+++ src/app/components/media/media.component.ts
@@ -9,26 +9,72 @@ import { Component } from '@angular/core';
 })
 export class MediaComponent {
 
+  private lastImageShow = 0;
+
   cardData = [
     {
       image: './assets/img/captures/low/image_01_low.jpg',
-      title: "Precios de un solo vistazo"
+      image_lager: './assets/img/captures/image_01_mid.jpg',
+      title: "Precios de un solo vistazo",
+      active: false
     },
     {
       image: './assets/img/captures/low/image_02_low.jpg',
-      title: "Establece alertas"
+      image_lager: './assets/img/captures/image_02_mid.jpg',
+      title: "Establece alertas",
+      active: false
     },
     {
       image: './assets/img/captures/low/image_07_low.jpg',
-      title: "Moodo oscuro"
+      image_lager: './assets/img/captures/image_07_mid.jpg',
+      title: "Moodo oscuro",
+      active: false
     },
     {
       image: './assets/img/captures/low/image_04_low.jpg',
-      title: "Como se crea tu energía"
+      image_lager: './assets/img/captures/image_04_mid.jpg',
+      title: "Como se crea tu energía",
+      active: false
     },
     {
       image: './assets/img/captures/low/image_05_low.jpg',
-      title: "Históarico diario"
+      image_lager: './assets/img/captures/image_05_mid.jpg',
+      title: "Históarico diario",
+      active: false
     },
-  ]
+  ];
+  
+
+  changeState(select: number): void {
+    switch(select) {
+      case 0:
+        this.cardData[0].active = !this.cardData[0].active
+        this.lastImageShow = 0
+      break;
+      case 1:
+        this.cardData[1].active = !this.cardData[1].active
+        this.lastImageShow = 1
+      break;
+      case 2:
+        this.cardData[2].active = !this.cardData[2].active
+        this.lastImageShow = 2
+      break;
+      case 3:
+        this.cardData[3].active = !this.cardData[3].active
+        this.lastImageShow = 3
+      break;
+      case 4:
+        this.cardData[4].active = !this.cardData[4].active
+        this.lastImageShow = 4
+      break;
+      default:
+        console.log("Error image number");
+    }
+  };
+
+  closeModal(): void {
+    this.changeState(this.lastImageShow);
+  }
+
+
 }
