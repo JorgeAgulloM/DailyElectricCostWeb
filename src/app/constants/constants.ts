88d01dbddce0b11227dcc51ec98f7b1c7c1f29a7
Jorge1 <agullojorge@gmail.com>
2024-03-07 08:17:17 +0100

    Added constants

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,9 @@
+export const TWITTER = 'twitter';
+export const YOUTUBE = 'youtube';
+export const INSTAGRAM = 'instagram';
+
+/** URLs */
+export const GOOGLE_PLAY_APP_LINK = 'https://play.google.com/store/apps/details?id=com.softyorch.dailyelectriccost';
+export const TWITTER_ACCOUNT_LINK = ''
+export const YOUTUBE_ACCOUNT_LINK = ''
+export const INSTAGRAM_ACCOUNT_LINK = ''
