7b5307fe8487a1bd715f0c571d76432485a29dcf
Jorge1 <agullojorge@gmail.com>
2024-03-10 17:49:10 +0100

    Implemented contact form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MediaComponent } from './components/media/media.component';
 import { AppFeaturesComponent } from './components/app-features/app-features.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactFromComponent } from './components/contact-from/contact-from.component';
+import { CommonActionsService } from './services/common-actions.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent, HeaderComponent, MediaComponent, AppFeaturesComponent, FooterComponent],
+  imports: [RouterOutlet, TopBarComponent, HeaderComponent, MediaComponent, AppFeaturesComponent, FooterComponent, ContactFromComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Electric Cost. Hello';
-  enabled: boolean = false;
+export class AppComponent implements OnInit {
 
-  courses: string[] = ['Angular', 'React', 'Spring Boot'];
+  showContactForm: boolean = false;
+  private subscription: Subscription | undefined;
 
-  setEnabled(): void { this.enabled = !this.enabled; }
+  constructor(private service: CommonActionsService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.service.getContactFormVisibility().subscribe((show: boolean) => {
+      console.log(`AppComponent -> ngOnInit`)
+      this.showContactForm = show;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
