798a33d2d6794fbf37ebca76104fc0b2b49ed8e2
Jorge1 <agullojorge@gmail.com>
2024-03-10 18:38:59 +0100

    Added validations

--- src/app/components/contact-from/contact-from.component.ts
+++ src/app/components/contact-from/contact-from.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonActionsService } from '../../services/common-actions.service';
 import { FormData } from '../../models/form-data';
 import { FormsModule } from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
+import Swal from 'sweetalert2';
 
 
 @Component({
   selector: 'app-contact-from',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, NgClass],
   templateUrl: './contact-from.component.html',
   styleUrl: './contact-from.component.css'
 })
