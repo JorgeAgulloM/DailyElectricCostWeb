4146c80f9216b20c83ebbf77f01f114788b312d8
Jorge1 <agullojorge@gmail.com>
2024-03-10 15:20:43 +0100

    Added privay policy link

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -7,3 +7,4 @@ export const GOOGLE_PLAY_APP_LINK = 'https://play.google.com/store/apps/details?
 export const TWITTER_ACCOUNT_LINK = 'https://twitter.com/JorgeAgulloM'
 export const YOUTUBE_ACCOUNT_LINK = 'https://www.youtube.com/watch?v=zAzbj_OGmZU&ab_channel=SoftYorch'
 export const INSTAGRAM_ACCOUNT_LINK = 'https://www.instagram.com/yorchdevelop'
+export const PRIVACY_POLICY_LINK = 'https://www.app-privacy-policy.com/live.php?token=7XCQyWrMdW0gyYoJGCeCVMgc54RexeH9'
