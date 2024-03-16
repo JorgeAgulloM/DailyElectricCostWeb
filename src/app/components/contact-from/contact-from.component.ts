dee38ce083e91b52b0ef7d51852b56d042332f16
Jorge1 <agullojorge@gmail.com>
2024-03-16 17:41:22 +0100

    Added loading swal

--- src/app/components/contact-from/contact-from.component.ts
+++ src/app/components/contact-from/contact-from.component.ts
@@ -4,7 +4,7 @@ import { BackendService } from "../../services/backend/backend.service";
 import { FormData } from "../../models/form-data";
 import { FormsModule } from "@angular/forms";
 import { NgClass, NgIf } from "@angular/common";
-import Swal from "sweetalert2";
+import Swal, { SweetAlertIcon } from "sweetalert2";
 
 @Component({
   selector: "app-contact-from",
@@ -33,20 +33,17 @@ export class ContactFromComponent {
   }
 
   onSubmit(): void {
+    Swal.fire({
+      title: "Contacto con Desarrollador!",
+      html: "Un momento por favor, estamos enviando su solicitud...",
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+
     this.backedSrv.sendContactDeveloper(this.formData).subscribe({
-      next: () =>
-        Swal.fire({
-          title: "Formulario enviado!",
-          text: "Gracias, nos pondremos en contacto lo antes posible.",
-          icon: "success",
-        }),
-      error: () =>
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Something went wrong!",
-          footer: '<a href="#">Why do I have this issue?</a>',
-        }),
+      next: (() => this.createSwalMessage('Gracias, nos pondremos en contacto lo antes posible.', 'success')),
+      error: (() => this.createSwalMessage('Ooops... Algo ha salido mal!', 'error'))
     });
 
     this.formData.email = "";
@@ -58,5 +55,13 @@ export class ContactFromComponent {
 
   closeContactForm(): void {
     this.service.toggleContactFormVisibility(false);
+  }
+
+  private createSwalMessage(text: string, icon: SweetAlertIcon): void {
+    Swal.fire({
+      title: "Contacto con Desarrollador!",
+      text: text,
+      icon: icon,
+    })
   }
 }
