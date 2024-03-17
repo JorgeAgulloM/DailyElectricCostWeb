d6602fcc4a28e8855d9516882afa99aaffc53a40
Jorge1 <agullojorge@gmail.com>
2024-03-17 20:30:58 +0100

    Config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SubscritionsComponent } from './pages/subscriptions/subscritions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'subscriptions/:code', component: SubscritionsComponent }
+];
