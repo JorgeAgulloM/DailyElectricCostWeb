79656fe1518637da2d9821a1d723d1e71d80a366
Jorge1 <agullojorge@gmail.com>
2024-03-17 20:32:37 +0100

    Extract home to home path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MediaComponent } from './components/media/media.component';
-import { AppFeaturesComponent } from './components/app-features/app-features.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ContactFromComponent } from './components/contact-from/contact-from.component';
-import { CommonActionsService } from './services/common-actions.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopBarComponent, HeaderComponent, MediaComponent, AppFeaturesComponent, FooterComponent, ContactFromComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-
-  showContactForm: boolean = false;
-  private subscription: Subscription | undefined;
-
-  constructor(private service: CommonActionsService) {}
-
-  ngOnInit(): void {
-    this.subscription = this.service.getContactFormVisibility().subscribe((show: boolean) => {
-      this.showContactForm = show;
-    })
-  }
-
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
-  }
-}
+export class AppComponent {}
