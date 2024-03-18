d48f95432c84b8410f42e20ed3b645b953ff3aec
Jorge1 <agullojorge@gmail.com>
2024-03-18 20:17:57 +0100

    Implemented activate/cancel subscription

--- src/app/pages/subscriptions/subscritions.component.ts
+++ src/app/pages/subscriptions/subscritions.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BackendService } from '../../services/backend/backend.service';
-import Swal, { SweetAlertIcon } from 'sweetalert2';
+import { CommonActionsService } from '../../services/common-actions.service';
 
 @Component({
   selector: 'app-subscritions',
@@ -12,18 +12,56 @@ import Swal, { SweetAlertIcon } from 'sweetalert2';
 export class SubscritionsComponent implements OnInit {
   @Input('code') code!: string;
   
-  
+  subscriberEmail: string = '';
   responseOfSubscription: string = '';
+  isCanceled: boolean = false;
 
-  constructor(private backendSrv: BackendService) {}
+  constructor(
+    private service: CommonActionsService,
+    private backendSrv: BackendService
+  ) {}
 
   ngOnInit(): void {
+    if (this.subscriberEmail == ''){
+      this.getEmail();
+      this.acivateSubscription();
+    }
+  }
+
+  cancelSubscrition(event: MouseEvent): void {
+    event.preventDefault();
+    this.backendSrv.sendCancelSubscriber(this.code).subscribe({
+      next: ((response) => {
+        this.isCanceled = true;
+        
+        if (typeof response?.message !== 'undefined') {
+          this.responseOfSubscription = response.message;
+        } else if (typeof response?.warning !== 'undefined') {
+          this.responseOfSubscription = response.warning;
+        } else if (typeof response?.error !== 'undefined') {
+          this.responseOfSubscription = response.error;
+        } else {
+          this.responseOfSubscription = response.error;
+        }
+      }),
+      error: ((error) => {
+        this.responseOfSubscription = error.error;
+      })
+    })
+  }
+
+  reactiveSubscription(event: MouseEvent): void {
+    event.preventDefault();
     this.acivateSubscription();
   }
 
-  //Buscar con el code el correo del usuario para mostrarlo
+  private getEmail(): void {
+    this.backendSrv.getEmailSubscriptor(this.code).subscribe({
+      next: (response => this.subscriberEmail = response.message)
+    });
+  }
 
-  acivateSubscription(): void {
+  private acivateSubscription(): void {
     this.backendSrv.sendActivateSubscriber(this.code).subscribe({
       next: ((response) => {
         if (typeof response?.message !== 'undefined') {
@@ -42,5 +80,14 @@ export class SubscritionsComponent implements OnInit {
     })
   }
 
+  goToPrivacyPolicy(event: MouseEvent): void {
+    event.preventDefault();
+    this.service.goToPrivacyPolicy();
+  }
+
+  goToGooglePlay(event: MouseEvent): void {
+    event.preventDefault();
+    this.service.goToGooglePlay();
+  }
 
 }
